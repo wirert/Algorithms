f17b8a351d48d32f6f0f7d0b594be8ab2233cb2b
Krasimir Nikolov <wirert@gmail.com>
2023-12-18 17:53:04 +0200

    01.02.RecursiveDrawing solution

--- Fundamentals/01. Recurtion and Backtracking - lab/01.Recursice Array Sum/Program.cs
+++ Fundamentals/01. Recurtion and Backtracking - lab/01.Recursice Array Sum/Program.cs
@@ -1,14 +1,14 @@
-﻿var array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+﻿var array = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
 
 
-Console.WriteLine(sumArr(array, 0));
+Console.WriteLine(SumArr(array, 0));
 
-static int sumArr(int[] array, int index)
+static int SumArr(int[] array, int index)
 {
     if (index == array.Length - 1)
     {
         return array[index];
     }
 
-    return sumArr(array, index + 1) + array[index];
+    return SumArr(array, index + 1) + array[index];
 }
---
+++ Fundamentals/01. Recurtion and Backtracking - lab/02. Recursive Drawing/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _02._Recursive_Drawing
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Draw(n);
+        }
+
+        private static void Draw(int n)
+        {
+            if( n == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', n));
+
+            Draw(n - 1);
+
+            Console.WriteLine(new string('#', n));
+        }
+    }
+
+}
