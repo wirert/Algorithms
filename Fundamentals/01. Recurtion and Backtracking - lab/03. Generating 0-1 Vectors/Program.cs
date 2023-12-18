b5b220d3702a91a5252957c6836db8aebc3070d4
Krasimir Nikolov <wirert@gmail.com>
2023-12-18 18:02:35 +0200

    01.03. Generating 0/1 Vectors solution

---
+++ Fundamentals/01. Recurtion and Backtracking - lab/03. Generating 0-1 Vectors/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _03._Generating_0_1_Vectors
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()!);
+
+            int[] arr = new int[n];
+
+            GenerataAndPrintVectors(arr, 0);
+        }
+
+        private static void GenerataAndPrintVectors(int[] arr, int index)
+        {
+            if (index == arr.Length)
+            {
+                Console.WriteLine(string.Join(string.Empty, arr));
+                return;
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                arr[index] = i;
+                GenerataAndPrintVectors(arr, index + 1);
+            }
+        }
+    }
+}
