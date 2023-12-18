f9bf26f61169937aeda0aa9395f5481c9a41edf7
Krasimir Nikolov <wirert@gmail.com>
2023-12-18 18:07:21 +0200

    01.04. Recursive Factorial

---
+++ Fundamentals/01. Recurtion and Backtracking - lab/04. Recursive Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _04._Recursive_Factorial
+{
+    using System;
+    using System.Numerics;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine(CalcFactorial(n));
+        }
+
+        private static BigInteger CalcFactorial(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            return n * CalcFactorial(n - 1);
+        }
+    }
+}
