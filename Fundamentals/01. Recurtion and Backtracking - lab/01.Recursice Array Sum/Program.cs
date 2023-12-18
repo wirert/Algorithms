7453626b695cb08c2ee844fb8a4daccfcc701122
Krasimir Nikolov <wirert@gmail.com>
2023-12-18 17:24:01 +0200

    01.01.Recursive array sum solution

---
+++ Fundamentals/01. Recurtion and Backtracking - lab/01.Recursice Array Sum/Program.cs
@@ -0,0 +1,14 @@
+﻿var array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+
+Console.WriteLine(sumArr(array, 0));
+
+static int sumArr(int[] array, int index)
+{
+    if (index == array.Length - 1)
+    {
+        return array[index];
+    }
+
+    return sumArr(array, index + 1) + array[index];
+}
