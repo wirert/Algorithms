7e6bc78a23e7923ad08e0593b69d52d6f0ee56d4
Krasimir Nikolov <wirert@gmail.com>
2023-12-22 17:42:36 +0200

    02. Nested Loops

---
+++ Fundamentals/03. Recursion and Combinatorial Problems - Exercise/02. Nested Loops/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _02._Nested_Loops
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var combination = new int[n];
+
+            PrintCombinations(combination, 0);
+        }
+
+        private static void PrintCombinations(int[] combination, int index)
+        {
+            if (index >= combination.Length)
+            {
+                Console.WriteLine(string.Join(" ", combination));
+                return;
+            }
+
+            for (int i = 1; i <= combination.Length; i++)
+            {
+                combination[index] = i;
+
+                PrintCombinations(combination, index + 1);
+            }
+        }
+    }
+}
