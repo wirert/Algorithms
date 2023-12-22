fa99b76fabbae73210c4bf4c9a50b076f308ce1b
Krasimir Nikolov <wirert@gmail.com>
2023-12-22 17:30:43 +0200

    01. Reverse Array solution

---
+++ Fundamentals/03. Recursion and Combinatorial Problems - Exercise/01. Reverse Array/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01._Reverse_Array
+{
+    using System;
+
+    internal class Program
+    {
+        private static string[] arr;
+
+        static void Main(string[] args)
+        {
+            arr = Console.ReadLine().Split(' ');
+
+            Reverse(0);
+        }
+
+        private static void Reverse(int index)
+        {
+            if (index >= arr.Length)
+            {
+                return;
+            }
+
+            Reverse(index + 1);
+
+            Console.Write(arr[index] + " ");
+        }
+    }
+}
