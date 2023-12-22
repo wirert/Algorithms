1e3837535fb7e4d2c0f8b0f459efa2589f292d1b
Krasimir Nikolov <wirert@gmail.com>
2023-12-22 12:25:34 +0200

    02. 01. Permutations without Repetition solution

---
+++ Fundamentals/02.Combinatorial Problems - Lab/01. Permutations without Repetition/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _01._Permutations_without_Repetition
+{
+    using System;
+
+    internal class Program
+    {
+        private static string[] elements;
+        private static string[] permutation;
+        private static bool[] isUsed;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(" ");
+            permutation = new string[elements.Length];
+            isUsed = new bool[elements.Length];
+
+            PrintPermutations(0);
+        }
+
+        private static void PrintPermutations(int index)
+        {
+            if (index >= permutation.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutation));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (!isUsed[i])
+                {
+                    isUsed[i] = true;
+                    permutation[index] = elements[i];
+
+                    PrintPermutations(index + 1);
+
+                    isUsed[i] = false;
+                }
+            }
+        }
+    }
+}
