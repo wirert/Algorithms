c9dc29cff961f497bd10d944ba3963680d63f119
Krasimir Nikolov <wirert@gmail.com>
2023-12-22 17:09:29 +0200

    Combinations with Repetition

---
+++ Fundamentals/02.Combinatorial Problems - Lab/06. Combinations with Repetition/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _06._Combinations_with_Repetition
+{
+    using System;
+
+    internal class Program
+    {
+        private static string[] elements;
+        private static string[] combination;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(' ');
+            int k = int.Parse(Console.ReadLine());
+            combination = new string[k];
+
+            FindCombinations(0, 0);
+        }
+
+        private static void FindCombinations(int index, int startIndex)
+        {
+            if (index >= combination.Length)
+            {
+                Console.WriteLine(string.Join(' ', combination));
+                return;
+            }
+
+            for (int i = startIndex; i < elements.Length; i++)
+            {
+                combination[index] = elements[i];
+
+                FindCombinations(index + 1, i);
+            }
+        }
+    }
+}
