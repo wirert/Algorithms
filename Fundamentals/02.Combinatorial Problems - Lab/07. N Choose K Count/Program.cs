f227140f02764164f580ee7dcba5a556ba77fa76
Krasimir Nikolov <wirert@gmail.com>
2023-12-22 17:20:40 +0200

    07. N Choose K Count

---
+++ Fundamentals/02.Combinatorial Problems - Lab/07. N Choose K Count/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _07._N_Choose_K_Count
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Binom(n, k));
+        }
+
+        private static long Binom(int row, int col)
+        {
+            if (row <= 1 || col == 0 || col == row)
+            {
+                return 1;
+            }
+
+            return Binom(row -1, col - 1) + Binom(row - 1, col);
+        }
+    }
+}
