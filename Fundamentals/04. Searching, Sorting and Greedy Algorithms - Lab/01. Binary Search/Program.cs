87a738f70a51e397a3d388c8332e9811b0c311bb
Krasimir Nikolov <wirert@gmail.com>
2024-01-05 19:28:27 +0200

    04.01.Binary search in sorted list

---
+++ Fundamentals/04. Searching, Sorting and Greedy Algorithms - Lab/01. Binary Search/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _01._Binary_Search
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static int[] arr;
+
+        static void Main(string[] args)
+        {
+            arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+            
+            int index = FindIndex(n, 0, arr.Length - 1);
+
+            Console.WriteLine(index);
+        }
+
+        private static int FindIndex(int n, int start, int end)
+        {
+            while (start <= end)
+            {
+                var middle = (start + end) / 2;
+
+                if (n == arr[middle])
+                {
+                    return middle;
+                }
+
+                if (n < arr[middle])
+                {
+                    end = middle - 1;
+                }
+                else
+                {
+                    start = middle + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
