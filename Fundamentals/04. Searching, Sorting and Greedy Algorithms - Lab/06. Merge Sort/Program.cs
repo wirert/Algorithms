28426e0b7117d27b2562c308eb4379de8108a4e2
Krasimir Nikolov <wirert@gmail.com>
2024-01-07 14:23:04 +0200

    04.06.Merge sort with better memory usage

--- Fundamentals/04. Searching, Sorting and Greedy Algorithms - Lab/06. Merge Sort/Program.cs
+++ Fundamentals/04. Searching, Sorting and Greedy Algorithms - Lab/06. Merge Sort/Program.cs
@@ -12,9 +12,68 @@
                  .Select(int.Parse)
                  .ToArray();
 
-            numbers = MergeSort(numbers);
+            numbers = BetterMergeSort(numbers);
 
             Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static int[] BetterMergeSort(int[] arr)
+        {
+            if (arr.Length <= 1)
+            {
+                return arr;
+            }
+
+            var copy = new int[arr.Length];
+            Array.Copy(arr, copy, arr.Length);
+
+            BetterMergeHelper(arr, copy, 0, arr.Length - 1);
+
+            return arr;
+        }
+
+        private static void BetterMergeHelper(int[] arr, int[] copy, int leftIdx, int rightIdx)
+        {
+            if (leftIdx >= rightIdx)
+            {
+                return;
+            }
+
+            int middleIdx = (leftIdx + rightIdx) / 2;
+
+            BetterMergeHelper(copy, arr, leftIdx, middleIdx);
+            BetterMergeHelper(copy, arr, middleIdx + 1, rightIdx);
+
+            MergeArrays(arr, copy, leftIdx, middleIdx, rightIdx);
+        }
+
+        private static void MergeArrays(int[] arr, int[] copy, int startIdx, int middleIdx, int endIdx)
+        {
+            int arrIdx = startIdx;
+            int leftIdx = startIdx;
+            int rightIdx = middleIdx + 1;
+
+            while (leftIdx <= middleIdx && rightIdx <= endIdx)
+            {
+                if (copy[leftIdx] < copy[rightIdx])
+                {
+                    arr[arrIdx++] = copy[leftIdx++];
+                }
+                else
+                {
+                    arr[arrIdx++] = copy[rightIdx++];
+                }
+            }
+
+            for (int i = leftIdx; i <= middleIdx; i++)
+            {
+                arr[arrIdx++] = copy[i];
+            }
+
+            for (int i = rightIdx; i <= endIdx; i++)
+            {
+                arr[arrIdx++] = copy[i];
+            }
         }
 
         private static int[] MergeSort(int[] arr)
