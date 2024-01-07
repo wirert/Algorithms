79fc7628afcb1429d60ee7eb1b0e581ed1d150d9
Krasimir Nikolov <wirert@gmail.com>
2024-01-07 11:39:04 +0200

    04.04. Insertion Sort

---
+++ Fundamentals/04. Searching, Sorting and Greedy Algorithms - Lab/04. Insertion Sort/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _04._Insertion_Sort
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                var j = i;
+
+                while (j > 0 && arr[j] < arr[j - 1])
+                {
+                   Swap(arr, j, j - 1);
+                    j--;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        private static void Swap(int[] arr, int i, int j)
+        {
+            var temp = arr[i]; 
+            arr[i] = arr[j]; 
+            arr[j] = temp;
+        }
+    }
+}
