8da5a61f76ce02b08a4144ce34fd879f5df56e98
Krasimir Nikolov <wirert@gmail.com>
2024-01-12 13:07:48 +0200

    07.01. Fibonacci

---
+++ Fundamentals/07.Introduction to Dynamic Programming - Lab/01. Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _01._Fibonacci
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        private static Dictionary<int, long> cache = new Dictionary<int, long>();
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()!);
+
+            if (n < 0) return;
+
+            Console.WriteLine(Fibonacci(n));
+        }
+
+        private static long Fibonacci(int n)
+        {
+            if(n == 0) return 0;
+
+            if(n == 1) return 1;
+
+            if (cache.ContainsKey(n)) return cache[n];
+
+            long result = Fibonacci(n -  1) + Fibonacci(n - 2);
+
+            cache[n] = result;
+
+            return result;
+        }
+    }
+}
