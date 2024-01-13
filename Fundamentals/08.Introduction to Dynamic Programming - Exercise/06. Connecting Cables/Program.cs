eb1fcc53cb679cf205c6c7730833f548bd12b22e
Krasimir Nikolov <wirert@gmail.com>
2024-01-13 16:15:10 +0200

    08.06. Connecting Cables

---
+++ Fundamentals/08.Introduction to Dynamic Programming - Exercise/06. Connecting Cables/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _06._Connecting_Cables
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cables1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int cableCount = cables1.Length;
+
+            var cables2 = cables1.OrderBy(x => x).ToArray();
+
+            var dpo = new int[cableCount + 1, cableCount + 1];
+
+            for (int row = 1; row <= cableCount; row++)
+            {
+                for (int col = 1; col <= cableCount; col++)
+                {
+                    if (cables1[row - 1] == cables2[col - 1])
+                    {
+                        dpo[row,col] = 1 + dpo[row - 1, col - 1];
+                    }
+                    else
+                    {
+                        dpo[row, col] = Math.Max(dpo[row, col - 1], dpo[row - 1, col]);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Maximum pairs connected: {dpo[cableCount, cableCount]}");
+        }
+    }
+}
