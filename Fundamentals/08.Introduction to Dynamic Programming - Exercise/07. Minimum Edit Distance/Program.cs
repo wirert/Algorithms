76b92386e8a45e2d832085df87184b3a878e79b7
Krasimir Nikolov <wirert@gmail.com>
2024-01-13 18:21:16 +0200

    08.07. Minimum Edit Distance

---
+++ Fundamentals/08.Introduction to Dynamic Programming - Exercise/07. Minimum Edit Distance/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _07._Minimum_Edit_Distance
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int replaceCost = int.Parse(Console.ReadLine()!);
+            int insertCost = int.Parse(Console.ReadLine()!);
+            int deleteCost = int.Parse(Console.ReadLine()!);
+
+            var modifiableStr = Console.ReadLine()!;
+            var templateStr = Console.ReadLine()!;
+
+            var dp = new int[modifiableStr.Length + 1, templateStr.Length + 1];
+
+            for (int row = 1; row < dp.GetLength(0); row++)
+            {
+                dp[row, 0] = row * deleteCost;
+            }
+
+            for (int col = 1; col < dp.GetLength(1); col++)
+            {
+                dp[0, col] = col * insertCost;
+            }
+
+            for (int row = 1; row < dp.GetLength(0); row++)
+            {
+                for (int col = 1; col < dp.GetLength(1); col++)
+                {
+                    if (modifiableStr[row - 1] == templateStr[col - 1])
+                    {
+                        dp[row, col] = dp[row - 1, col - 1];
+                    }
+                    else
+                    {
+                        var replace = dp[row - 1, col - 1] + replaceCost;
+                        var delete = dp[row - 1, col] + deleteCost;
+                        var insert = dp[row, col - 1] + insertCost;
+
+                        dp[row, col] = Math.Min(Math.Min(replace, delete), insert);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Minimum edit distance: {dp[modifiableStr.Length, templateStr.Length]}");
+        }
+    }
+}
