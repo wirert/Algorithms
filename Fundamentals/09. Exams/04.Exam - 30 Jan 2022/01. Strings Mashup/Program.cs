11e79248ea1f632833fd2a08e16ec43c6972294a
Krasimir Nikolov <wirert@gmail.com>
2024-01-15 19:24:23 +0200

    09.04.01. Strings Mashup

---
+++ Fundamentals/09. Exams/04.Exam - 30 Jan 2022/01. Strings Mashup/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _01._Strings_Mashup
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        private static List<string> variations;
+
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine()!.ToCharArray().OrderBy(c => c).ToArray();
+            int k = int.Parse(Console.ReadLine()!);
+
+            variations = new List<string>();            
+
+            FindVariations(0, string.Empty, str, k);
+
+            Console.WriteLine(string.Join(Environment.NewLine, variations));
+        }
+
+        private static void FindVariations(int idx, string variation, char[] str, int varLength)
+        {
+            if (variation.Length >= varLength)
+            {
+                variations.Add(variation);
+                return;
+            }
+
+            for (int i = idx; i < str.Length; i++)
+            {
+                var newVariation = variation + str[i];
+
+                FindVariations(i, newVariation, str, varLength);
+            }
+        }
+    }
+}
