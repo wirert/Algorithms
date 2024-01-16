28de998a54f22e66a68b90e63221b39084f5ac06
Krasimir Nikolov <wirert@gmail.com>
2024-01-16 12:26:13 +0200

    09.05.03. Strings Mashup

---
+++ Fundamentals/09. Exams/05.Exam - 23 Jan 2022/03. Strings Mashup/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _03._Strings_Mashup
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var chars = Console.ReadLine()!.ToUpper().ToCharArray();
+
+            PrintMashups(chars, 0);
+        }
+
+        private static void PrintMashups(char[] chars, int idx)
+        {
+            if (idx >= chars.Length)
+            {
+                Console.WriteLine(string.Join("", chars));
+                return;
+            }
+
+            PrintMashups(chars, idx + 1);
+
+            var original = chars[idx];
+            chars[idx] = char.Parse(chars[idx].ToString().ToLower());
+
+            if(original != chars[idx])
+            {
+                PrintMashups(chars, idx + 1);
+                chars[idx] = original;
+            }
+        }
+    }
+}
