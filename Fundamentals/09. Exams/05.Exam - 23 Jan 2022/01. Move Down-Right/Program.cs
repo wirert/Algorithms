a89b7e2dc6b405f0c8f5012a9baf13220e7f1d0c
Krasimir Nikolov <wirert@gmail.com>
2024-01-16 11:17:23 +0200

    09.05.01. Move Down / Right (with dinamic optimisation)

---
+++ Fundamentals/09. Exams/05.Exam - 23 Jan 2022/01. Move Down-Right/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _01._Move_Down_Right
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine()!);
+            int cols = int.Parse(Console.ReadLine()!);
+
+            var dp = new long[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                dp[row, 0] = 1;
+            }
+
+            for (int col = 0; col < cols; col++)
+            {
+                dp[0, col] = 1;
+            }
+
+            for (int row = 1; row < rows; row++)
+            {
+                for (int col = 1; col < cols; col++)
+                {
+                    dp[row, col] = dp[row - 1, col] + dp[row, col - 1];
+                }
+            }
+
+            long pathsCount = dp[rows - 1, cols - 1];
+
+            Console.WriteLine(pathsCount);
+        }
+    }
+}
