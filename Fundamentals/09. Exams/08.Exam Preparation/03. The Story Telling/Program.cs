d48e42589ae49c58d64350efb1649ae02ca934c6
Krasimir Nikolov <wirert@gmail.com>
2024-01-17 21:54:24 +0200

    09.08.03. The Story Telling

---
+++ Fundamentals/09. Exams/08.Exam Preparation/03. The Story Telling/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _03._The_Story_Telling
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        private static Dictionary<string, List<string>> storyGraph = new Dictionary<string, List<string>>();
+        private static HashSet<string> visited = new HashSet<string>();
+        private static HashSet<string> cycles = new HashSet<string>();
+
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine()!.Split(" ->", StringSplitOptions.RemoveEmptyEntries);
+
+            while (line[0] != "End")
+            {
+                string node = line[0].Trim();
+
+                if (line.Length > 1)
+                {
+                    var children = line[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                    storyGraph.Add(node, children);
+                }
+                else
+                {
+                    storyGraph.Add(node, new List<string>());
+                }
+
+                line = Console.ReadLine()!.Split(" ->", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            Stack<string> sortedStory = new Stack<string>();
+
+            try
+            {
+                foreach (var chapter in storyGraph.Keys)
+                {
+                    SortStory(chapter, sortedStory);
+                }
+
+                Console.WriteLine(string.Join(" ", sortedStory));
+            }
+            catch (InvalidOperationException ioe)
+            {
+                Console.WriteLine(ioe.Message);
+            }
+        }
+
+        private static void SortStory(string node, Stack<string> sortedStory)
+        {
+            if (cycles.Contains(node))
+            {
+                throw new InvalidOperationException("Invalid sorting");
+            }
+
+            if (visited.Contains(node))
+            {
+                return;
+            }
+
+            visited.Add(node);
+            cycles.Add(node);
+
+            foreach (var childChapter in storyGraph[node])
+            {
+                SortStory(childChapter, sortedStory);
+            }
+
+            sortedStory.Push(node);
+            cycles.Remove(node);
+        }
+    }
+}
